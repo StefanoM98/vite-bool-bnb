0519eaa84e5241d696e399dbbb7f686f2f9dcdbf
LucaPertosa <luca.pertosa@hotmail.it>
2023-07-12 21:52:19 +0200

    added login and register links in header

--- src/components/Header/AppHeader.vue
+++ src/components/Header/AppHeader.vue
@@ -42,13 +42,12 @@ export default {
                             <div class="d-flex align-items-center text-center justify-content-center">
                                 <li class="nav-item dropdown">
                                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        Dropdown
+                                        User Actions
                                     </a>
                                     <ul class="dropdown-menu">
-                                        <li><a class="dropdown-item" href="#">Action</a></li>
-                                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                                        <li><a class="dropdown-item" href="http://localhost:8000/login">Login</a></li>
                                         <li><hr class="dropdown-divider"></li>
-                                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                        <li><a class="dropdown-item" href="http://localhost:8000/register">Register</a></li>
                                     </ul>
                                 </li>
                             </div>
